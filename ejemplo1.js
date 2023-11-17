24a05041a5a789d8fe261c9b2007b76704620530
SSBC185 <sam.barrioscabrera@gmail.com>
2023-11-17 12:55:15 -0300

    Agreguea el archivo ejemplo1.js

---
+++ ejemplo1.js
@@ -0,0 +1,12 @@
+const mascota = {
+    nombre:"Bony",
+    tipoDeMascota:"Perro",
+    color:"Dorado",
+    raza:"Golden retriever"
+}
+
+const {nombre,tipoDeMascota,color,raza} = mascota;
+
+const mensaje = `Hola les presento a mi mascota su nombre es: ${nombre}, es un hermoso ${tipoDeMascota}, de color: ${color} y su raza es: ${raza}.`
+
+//console.log(mensaje);
